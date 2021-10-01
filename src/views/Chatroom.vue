e8893d584aaa132e55e903e6fad3029c3fcd2e32
aestas <hawhawba@gmail.com>
2021-10-01 20:14:16 +0900

    second commit

---
+++ src/views/Chatroom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="chatroom">
+    <h1>Chatroom</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chatroom',
+  components: {}
+}
+</script>
+
+<style>
+@import '../src/css/chatroom.css';
+</style>
+
